9954dd0490b69e160e566d202e12aa85c98010aa
Ramazan Aminov <RAminov@inno.tech>
2026-02-18 19:50:33 +0300

    Задание #2. JDBC Тесты. Удаление отдела

---
+++ test/ru/stepup/jdbc/OfficeTests.java
@@ -0,0 +1,31 @@
+package ru.stepup.jdbc;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class OfficeTests {
+    @Test
+    @DisplayName("Проверка удаления информации о сотрудниках после удаления отдела")
+    void testEmployeeDataRemovalAfterDepartmentDeletion() {
+        try (Connection con = DriverManager.getConnection("jdbc:h2:.\\Office")) {
+            PreparedStatement pStm = con.prepareStatement("SELECT EXISTS(SELECT 1 FROM Employee WHERE DepartmentId = ?)");
+            pStm.setInt(1, 2);
+            pStm.executeQuery();
+            ResultSet rs = pStm.getResultSet();
+            rs.next();
+            assertThat("Система не соответствует требованиям, т.к после удаления отдела информация о сотрудниках " +
+                    "отдела не удалена из таблицы Employee,", rs.getBoolean(1), equalTo(false));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
